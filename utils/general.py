99b115707a379ac30ee19ba4fb98f98783e9c99a
iliya <iliya2012hajjar@gmail.com>
2025-01-12 23:39:11 +0330

    Remove __pycache__ directories from repository

--- utils/general.py
+++
@@ -1,5 +0,0 @@
-from django.core.cache import cache
-
-def invalidate_cache(article_id):
-    cache.delete(f"article_{article_id}_num_scores")
-    cache.delete(f"article_{article_id}_average_score")
